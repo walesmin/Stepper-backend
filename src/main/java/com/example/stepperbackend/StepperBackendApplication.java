0bf2a6f3c34002b9fe36dbfd70b140c0d317d1a9
hyojin425 <gywls5184@gmail.com>
2024-08-13 18:16:52 +0900

    [Fix] application.yml 오타 수정

--- src/main/java/com/example/stepperbackend/StepperBackendApplication.java
+++ src/main/java/com/example/stepperbackend/StepperBackendApplication.java
@@ -2,10 +2,8 @@ package com.example.stepperbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource(value = {"env.properties"})
 public class StepperBackendApplication {
 
 	public static void main(String[] args) {
