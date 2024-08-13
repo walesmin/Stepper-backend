2d4ec76e41ddb313f5b708345718537a746b79a9
hyojin425 <gywls5184@gmail.com>
2024-08-13 18:04:46 +0900

    [Fix] @PropertySource 적용

--- src/main/java/com/example/stepperbackend/StepperBackendApplication.java
+++ src/main/java/com/example/stepperbackend/StepperBackendApplication.java
@@ -2,8 +2,10 @@ package com.example.stepperbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = {"env.properties"})
 public class StepperBackendApplication {
 
 	public static void main(String[] args) {
